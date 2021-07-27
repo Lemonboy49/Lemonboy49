9e15a736d8d925c98b6d911a0fa8afb8e1ba4c5f
Lemonboy49 <71829601+Lemonboy49@users.noreply.github.com>
2021-07-27 16:03:08 +0545

    Add files via upload
    Added functions to "Again" btn.

---
+++ script.js
@@ -0,0 +1,71 @@
+"use strict";
+/*
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Number! 🎉 ";
+
+document.querySelector(".number").textContent = 13;
+
+document.querySelector(".score");
+
+document.querySelector(".guess").value = 13;
+console.log(document.querySelector(".guess").value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+let score = 20;
+
+const textScore = function () {
+  document.querySelector(".score").textContent = score;
+};
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  document.querySelector(".again").addEventListener("click", function () {
+    score = 20;
+    textScore();
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector(".message").textContent = "Start guessing...";
+    document.querySelector(".guess").value = "";
+    document.querySelector(".number").textContent = "?";
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    console.log(secretNumber);
+  });
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No Number";
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number! 🥳";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
+
+    // When guess is High
+  } else if (guess > secretNumber) {
+    if (score >= 1) {
+      document.querySelector(".message").textContent = "Too High! 📈";
+      score--;
+      textScore();
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+    }
+
+    // When guess is Low
+  } else if (guess < secretNumber) {
+    if (score >= 1) {
+      document.querySelector(".message").textContent = "Too Low! 📉";
+      score--;
+      textScore();
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+    }
+  }
+});
+
+console.log(secretNumber);
